0a766947721128a6e38e6d7f684eea7caeded24a
Jae Cho <jjaecho@gmail.com>
2022-06-01 22:42:18 -0400

    Initial Commit

---
+++ P11_Bookworm/P11_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  P11_BookwormApp.swift
+//  P11_Bookworm
+//
+//  Created by Jae Cho on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct P11_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
